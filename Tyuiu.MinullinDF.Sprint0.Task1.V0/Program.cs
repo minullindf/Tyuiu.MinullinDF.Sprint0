0d2acec6f8840e40f66fcc5dc8d66958084953a6
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-09-10 10:37:29 +0500

    + task1.v0

---
+++ Tyuiu.MinullinDF.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Динар";
+        int age = 10;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
