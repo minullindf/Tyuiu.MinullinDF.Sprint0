7bba4be718de22446c57ac4f71f3c6288fec58ce
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-09-10 18:34:04 +0500

    Task3

---
+++ Tyuiu.MinullinDF.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿using Tyuiu.MinullinDF.Sprint0.Task3.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Sum(7, 7));
+        Console.ReadKey();
+    }
+}
