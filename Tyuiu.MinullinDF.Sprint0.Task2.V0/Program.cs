51a58a8586fba6d4209cd432fecabdb1592469e8
Dinar Minullin <Tyuiu.MinullinDF@outlook.com>
2025-09-10 17:33:24 +0500

    + Task2

---
+++ Tyuiu.MinullinDF.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.MinullinDF.Sprint0.Task2.V0.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Динар"));
+        Console.ReadKey();
+        Console.ReadKey();
+    }
+}
